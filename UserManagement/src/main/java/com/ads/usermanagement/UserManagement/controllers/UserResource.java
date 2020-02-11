adb59816c79559953154e3465ea04f4570f0d130
Dhamejani, Drishti Vijay <ddhameja@iu.edu>
2020-02-11 17:09:12 -0500

    Added CORS in UserManagement

--- UserManagement/src/main/java/com/ads/usermanagement/UserManagement/controllers/UserResource.java
+++ UserManagement/src/main/java/com/ads/usermanagement/UserManagement/controllers/UserResource.java
@@ -8,6 +8,7 @@ import com.ads.usermanagement.UserManagement.util.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
