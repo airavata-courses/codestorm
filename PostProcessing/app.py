9c10e0a851e1c9cf729f1533d4c0f789746b38bd
Daksha Maruti Nagre <39939579+DakshaNagre@users.noreply.github.com>
2020-02-12 17:16:13 -0500

    PostProcessing Microservice latest

---
+++ PostProcessing/app.py
@@ -0,0 +1,25 @@
+# importing reuired libraries
+from flask import Flask
+from flask_cors import CORS,cross_origin
+import post_processing_consumer as ppc
+
+# initializing flask application
+app = Flask(__name__, static_url_path='/static')
+app.config.from_object(__name__)
+
+# enabling cross origin access to any source
+# Once the applciation is deployed, the '*' will be replaced with
+# source URL
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route("/postprocessing")
+def helper():
+    image_link = ppc.pp_consumer()
+    print("Got link object in the helper of app.py of post processing!!!!! ------> ", image_link)
+    return image_link
+
+# Starting the application server
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0', port = 5500)
