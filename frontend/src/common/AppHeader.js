41f7060c3c9f159948819f634d60f7522c9e2543
Dhamejani, Drishti Vijay <ddhameja@iu.edu>
2020-03-01 14:27:47 -0500

    Front end added to version2

---
+++ frontend/src/common/AppHeader.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import {
+    Link,
+    withRouter
+} from 'react-router-dom';
+import './AppHeader.css';
+import { Layout, Menu, Dropdown, Icon } from 'antd';
+
+const Header = Layout.Header;
+
+    
+class AppHeader extends Component {
+    constructor(props) {
+        console.log("AppHeader Component constructor")
+        super(props);   
+        this.handleMenuClick = this.handleMenuClick.bind(this);   
+    }
+
+    handleMenuClick({ key }) {
+      console.log("AppHeader Component handleMenuClick")
+      if(key === "logout") {
+        this.props.onLogout();
+      }
+    }
+
+    render() {
+        console.log("AppHeader Component render")
+        let menuItems;
+        if(this.props.currentUser) {
+          menuItems = [
+            <Menu.Item key="/">
+              <Link to="/">
+                <Icon type="home" className="nav-icon" />
+              </Link>
+            </Menu.Item>,
+          <Menu.Item key="/profile" className="profile-menu">
+                <ProfileDropdownMenu 
+                  currentUser={this.props.currentUser} 
+                  handleMenuClick={this.handleMenuClick}/>
+            </Menu.Item>
+          ]; 
+        } else {
+          menuItems = [
+            <Menu.Item key="/login">
+              <Link to="/login">Login</Link>
+            </Menu.Item>,
+            <Menu.Item key="/signup">
+              <Link to="/signup">Signup</Link>
+            </Menu.Item>                  
+          ];
+        }
+
+        return (
+            <Header className="app-header">
+            <div className="container">
+              <div className="app-title" >
+                
+              </div>
+              <Menu
+                className="app-menu"
+                mode="horizontal"
+                selectedKeys={[this.props.location.pathname]}
+                style={{ lineHeight: '64px' }} >
+                  {menuItems}
+              </Menu>
+            </div>
+          </Header>
+        );
+    }
+}
+
+function ProfileDropdownMenu(props) {
+
+  console.log("AppHeader Component render")
+
+  const dropdownMenu = (
+    <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
+      <Menu.Divider />
+      <Menu.Item key="/weatherdata" className="dropdown-item">
+        <Link to="/weatherdata" >Predict Weather Data</Link>
+      </Menu.Item>
+      <Menu.Item key="logout" className="dropdown-item">
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Dropdown 
+      overlay={dropdownMenu} 
+      trigger={['click']}
+      getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
+      <a className="ant-dropdown-link">
+         <Icon type="user" className="nav-icon" style={{marginRight: 0}} /> <Icon type="down" />
+      </a>
+    </Dropdown>
+  );
+}
+
+
+export default withRouter(AppHeader);
